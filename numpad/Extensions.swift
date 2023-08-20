a37d83bd32c856cb105027e090fe285017e433bf
Constantin Clerc <constclerc@gmail.com>
2023-08-20 09:02:12 +0200

    setup

--- numpad/Extensions.swift
+++ numpad/Extensions.swift
@@ -44,3 +44,44 @@ func sendNumToServ(intValue: Int, ipAddress: String) {
        }
        task.resume()
 }
+
+// buttons by haxi0
+
+public struct CustomButtonStyle: ButtonStyle {
+    public func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .font(.body.weight(.medium))
+            .padding(.vertical, 12)
+            .foregroundColor(.white)
+            .frame(maxWidth: .infinity)
+            .background(RoundedRectangle(cornerRadius: 14.0, style: .continuous)
+                            .fill(Color.accentColor))
+            .opacity(configuration.isPressed ? 0.4 : 1.0)
+    }
+}
+
+public struct LinkButtonStyle: ButtonStyle {
+    public func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .font(.body.weight(.medium))
+            .padding(.vertical, 12)
+            .foregroundColor(.accentColor)
+            .frame(maxWidth: .infinity)
+            .background(RoundedRectangle(cornerRadius: 12.0, style: .continuous)
+                            .fill(Color.accentColor.opacity(0.1)))
+            .opacity(configuration.isPressed ? 0.4 : 1.0)
+    }
+}
+
+public struct DangerButtonStyle: ButtonStyle {
+    public func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .font(.headline.weight(.bold))
+            .padding(.vertical, 12)
+            .foregroundColor(.red)
+            .frame(maxWidth: .infinity)
+            .background(RoundedRectangle(cornerRadius: 12.0, style: .continuous)
+                            .fill(Color.red.opacity(0.1)))
+            .opacity(configuration.isPressed ? 0.4 : 1.0)
+    }
+}
--- numpad/numpadApp.swift
+++ numpad/numpadApp.swift
@@ -19,15 +19,19 @@ struct numpadApp: App {
     @State private var qrScannerSheet = false
     @State private var manualIPSheet = false
     @State private var firsTime = false
+    @AppStorage("hasLaunchedBefore") var hasLaunchedBefore: Bool = false
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .onAppear{
-                    if UserDefaults.standard.bool(forKey: "hasLaunchedBefore") == false {
-                        
-                        UserDefaults.standard.set(true, forKey: "hasLaunchedBefore")
+                    if !hasLaunchedBefore {
+                        firsTime.toggle()
                     }
-                    qrScannerSheet.toggle()
+                    while hasLaunchedBefore == true {
+                    }
+                    if hasLaunchedBefore == true {
+                        qrScannerSheet.toggle()
+                    }
                 }
                 .sheet(isPresented: $qrScannerSheet) {
                     VStack {
@@ -93,7 +97,84 @@ struct numpadApp: App {
                     .interactiveDismissDisabled(true)
                 }
                 .sheet(isPresented: $firsTime) {
-                    
+                    NavigationView {
+                        List {
+                            Section(header: Text("Welcome!")) {
+                                Text("Numpad is an app made by diyara3 (numpad touches) and c22dev (back-end, setup, qr-code, server) that allows you to use your phone as a numpad on your computer. Please note this doesn't work on Linux right now due to a node module limitation.")
+                            }
+                            
+                            Section(header: Text("Step 1")) {
+                                Text("Download the latest node.js build on your computer and install it (windows only). If you're on macOS, make sure brew is installed and run :")
+                                VStack {
+                                    Text("brew install npm")
+                                        .font(.custom("Menlo", size: 15))
+                                        .foregroundColor(.white)
+                                        .padding()
+                                }
+                                .textSelection(.enabled)
+                                .background (
+                                    Color.black
+                                        .cornerRadius(5)
+                                )
+                                Button("Open website") {
+                                    UIApplication.shared.open(URL(string: "https://nodejs.org/en/download")!)
+                                }
+                                .buttonStyle(LinkButtonStyle())
+                            }
+                            
+                            Section(header: Text("Step 2")) {
+                                Text("Download repo as zip on your PC/Mac and cd to the directory using your computer's terminal.")
+                                Button("Open repo") {
+                                    UIApplication.shared.open(URL(string: "https://github.com/diyar2137237243/numpad")!)
+                                }
+                                .buttonStyle(LinkButtonStyle())
+                            }
+                            
+                            Section(header: Text("Step 3")) {
+                                Text("Install dependencies by running this exact command.")
+                                VStack {
+                                    Text("npm install express robotjs qrcode node-pty")
+                                        .font(.custom("Menlo", size: 15))
+                                        .foregroundColor(.white)
+                                        .padding()
+                                }
+                                .textSelection(.enabled)
+                                .background (
+                                    Color.black
+                                        .cornerRadius(5)
+                                )
+                            }
+                            
+                            Section(header: Text("Step 4")) {
+                                Text("Launch the server.")
+                                VStack {
+                                    Text("node server.js")
+                                        .font(.custom("Menlo", size: 15))
+                                        .foregroundColor(.white)
+                                        .padding()
+                                }
+                                .textSelection(.enabled)
+                                .background (
+                                    Color.black
+                                        .cornerRadius(5)
+                                )
+                            }
+                            
+                            Section(header: Text("Step 5")) {
+                                Text("Congratulations! If you haven't encountered any errors, you have finished the setup. To restart everything, you'll have to repeat steps 2 (only cd part) and 4.")
+                            }
+                        }
+                        .navigationTitle("Setup")
+                        .environment(\.defaultMinListRowHeight, 50)
+                    }
+                        .interactiveDismissDisabled(true)
+                    Button("Done !") {
+                        hasLaunchedBefore = true
+                        firsTime = false
+                        qrScannerSheet.toggle()
+                    }
+                        .buttonStyle(DangerButtonStyle())
+                        .padding()
                 }
         }
     }
