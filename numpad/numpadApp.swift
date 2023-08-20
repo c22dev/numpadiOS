98ce815472aa258bf592d4da5be1004e9da13ba9
Constantin Clerc <constclerc@gmail.com>
2023-08-20 08:31:19 +0200

    no qr code ? enter text

--- numpad/numpadApp.swift
+++ numpad/numpadApp.swift
@@ -18,10 +18,15 @@ struct numpadApp: App {
     @State var ip2: String = ""
     @State private var qrScannerSheet = false
     @State private var manualIPSheet = false
+    @State private var firsTime = false
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .onAppear{
+                    if UserDefaults.standard.bool(forKey: "hasLaunchedBefore") == false {
+                        
+                        UserDefaults.standard.set(true, forKey: "hasLaunchedBefore")
+                    }
                     qrScannerSheet.toggle()
                 }
                 .sheet(isPresented: $qrScannerSheet) {
@@ -47,17 +52,48 @@ struct numpadApp: App {
                         .padding(.top, 8)
                         .sheet(isPresented: $manualIPSheet) {
                             VStack{
+                                Text("Please input IP Adress to continue")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                    .multilineTextAlignment(.center)
+                                    .padding()
+                                Text("You can press ',' for dots in your IP adress.")
+                                    .font(.footnote)
+                                    .foregroundColor(.secondary)
+                                    .padding(.bottom)
+                                    .multilineTextAlignment(.center)
                                 TextField("IP", text: $ip2)
+                                    .keyboardType(.decimalPad)
+                                    .onChange(of: ip2) { newValue in
+                                        ip2 = newValue.replacingOccurrences(of: ",", with: ".")
+                                    }
+                                    .padding(10)
+                                    .background(
+                                        RoundedRectangle(cornerRadius: 10)
+                                        .stroke(Color.gray, lineWidth: 1)
+                                    )
+                                    .frame(width: 300)
+                                    .multilineTextAlignment(.center)
                                 Button("Done") {
                                     ip = ip2
                                     manualIPSheet = false
                                     qrScannerSheet = false
                                 }
+                                .padding()
                             }
+                            Button("or scan QR code...") {
+                                manualIPSheet = false
+                            }
+                            .font(.footnote)
+                            .foregroundColor(.blue)
+                            .padding(.top, 8)
                             .interactiveDismissDisabled(true)
                         }
                     }
                     .interactiveDismissDisabled(true)
+                }
+                .sheet(isPresented: $firsTime) {
+                    
                 }
         }
     }
